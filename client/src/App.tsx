80f8934b0f27b75e4d48902681f1366a38472e2d
Philipp Arndt <2f.mail@gmx.de>
2020-09-05 14:07:30 +0200

    add simple client demo

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import StompDemo from './StompDemo';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <StompDemo/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/StompDemo.tsx
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from 'react'
+import { Client, Message } from '@stomp/stompjs'
+import { v4 as uuidv4 } from 'uuid'
+
+function StompDemo() {
+    const [client, setClient] = useState<Client | undefined>(undefined)
+    const [messages, setMessages] = useState<Message[]>([])
+
+    useEffect(() => {
+        if (client) {
+            const subscription = client.subscribe("/topic/general", message => {
+                console.log(messages)
+                setMessages([...messages, message])
+            })
+
+            return () => subscription.unsubscribe()
+        }
+    }, [client, messages])
+
+    useEffect(() => {
+        const client = new Client({
+            brokerURL: "ws://localhost:61614",
+            connectHeaders: {
+            },
+            debug: function (str: string) {
+              console.log(str);
+            },
+            reconnectDelay: 5000,
+            heartbeatIncoming: 4000,
+            heartbeatOutgoing: 4000
+          })
+
+          client.onConnect = function(frame) {
+            setClient(client)
+          }
+          
+          client.onStompError = function (frame) {
+            console.log('Broker reported error: ' + frame.headers['message']);
+            console.log('Additional details: ' + frame.body);
+          }
+          
+          client.activate()
+
+          return () => client.deactivate()
+    },[])
+
+    let sendButton
+    if (client) {
+        sendButton = <button onClick={e => {
+            client.publish({destination: '/topic/general', body: `Hello world ${uuidv4()}` });
+        }}>Send</button>
+    }
+    else {
+        sendButton = <p>no client available.</p>
+    }
+
+    const messageList = messages.map(message => <li key={message.body}>{message.body}</li>)
+
+    return (
+        <>
+        <div>Stomp! {sendButton}</div>
+        <div>{messageList}</div>
+        </>
+    )
+}
+
+export default StompDemo

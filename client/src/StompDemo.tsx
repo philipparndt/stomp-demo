11d86b408be9f93a13a942ea8d74dddaa3c431de
Philipp Arndt <2f.mail@gmx.de>
2020-09-05 18:53:38 +0200

    rxjs

--- client/src/StompDemo.tsx
+++ client/src/StompDemo.tsx
@@ -1,21 +1,30 @@
 import React, { useEffect, useState } from 'react'
 import { Client } from '@stomp/stompjs'
 import { v4 as uuidv4 } from 'uuid'
+import { useSubscribe, Subscribe } from "use-subscribe"
+import { interval, Subject, Observable } from "rxjs"
 
 type Msg = {
     id: string
     body: string
 }
 
+const intCounter = interval(1000);
+
+var emitter: { next: (arg0: string) => void };
+const observable = Observable.create((e: any) => emitter = e);
+
 function StompDemo() {
     const [client, setClient] = useState<Client | undefined>(undefined)
     const [messages, setMessages] = useState<Msg[]>([])
+    const obsCount = useSubscribe(intCounter, 0);
+    const s = useSubscribe(observable, 0);
 
     useEffect(() => {
         if (client) {
             const subscription = client.subscribe("/topic/general", message => {
-                console.log(messages)
-                setMessages([...messages, {id: uuidv4(), body: message.body}])
+                emitter.next(message.body);
+                // setMessages([...messages, {id: uuidv4(), body: message.body}])
             })
 
             return () => subscription.unsubscribe()
@@ -52,7 +61,9 @@ function StompDemo() {
     let sendButton
     if (client) {
         sendButton = <button onClick={e => {
-            client.publish({destination: '/topic/general', body: `Hello world ${uuidv4()}` });
+            for (let i = 0; i < 10; i++) {
+                client.publish({destination: '/topic/general', body: `Hello world ${i} ${uuidv4()}` });
+            }
         }}>Send</button>
     }
     else {
@@ -63,6 +74,8 @@ function StompDemo() {
 
     return (
         <>
+        <div>{s}</div>
+        <div>tick: {obsCount}</div>
         <div>Stomp! {sendButton}</div>
         <div>{messageList}</div>
         </>

5d29982aced0c2fa2ca56ff80c392a42760e9c58
Philipp Arndt <2f.mail@gmx.de>
2020-09-05 19:32:13 +0200

    basic styling

--- client/src/StompDemo.tsx
+++ client/src/StompDemo.tsx
@@ -1,85 +1,106 @@
-import React, { useEffect, useState } from 'react'
-import { Client } from '@stomp/stompjs'
-import { v4 as uuidv4 } from 'uuid'
-import { useSubscribe, Subscribe } from "use-subscribe"
-import { interval, Subject, Observable } from "rxjs"
+import React, { useEffect, useState } from "react";
+import { Client } from "@stomp/stompjs";
+import { useSubscribe } from "use-subscribe";
+import { interval, Observable } from "rxjs";
 
 type Msg = {
-    id: string
-    body: string
-}
+  id: string;
+  body: string;
+};
 
-const intCounter = interval(1000);
+const intCounter = interval(100);
 
 var emitter: { next: (arg0: string) => void };
-const observable = Observable.create((e: any) => emitter = e);
+const observable = Observable.create((e: any) => (emitter = e));
 
 function StompDemo() {
-    const [client, setClient] = useState<Client | undefined>(undefined)
-    const [messages, setMessages] = useState<Msg[]>([])
-    const obsCount = useSubscribe(intCounter, 0);
-    const s = useSubscribe(observable, 0);
+  const [client, setClient] = useState<Client | undefined>(undefined);
+  const obsCount = useSubscribe(intCounter, 0);
+  const current = useSubscribe(observable, 0);
 
-    useEffect(() => {
-        if (client) {
-            const subscription = client.subscribe("/topic/general", message => {
-                emitter.next(message.body);
-                // setMessages([...messages, {id: uuidv4(), body: message.body}])
-            })
-
-            return () => subscription.unsubscribe()
-        }
-    }, [client, messages])
-
-    useEffect(() => {
-        const client = new Client({
-            brokerURL: "ws://localhost:61614",
-            connectHeaders: {
-            },
-            debug: function (str: string) {
-              console.log(str);
-            },
-            reconnectDelay: 5000,
-            heartbeatIncoming: 4000,
-            heartbeatOutgoing: 4000
-          })
-
-          client.onConnect = function(frame) {
-            setClient(client)
-          }
-          
-          client.onStompError = function (frame) {
-            console.log('Broker reported error: ' + frame.headers['message']);
-            console.log('Additional details: ' + frame.body);
-          }
-          
-          client.activate()
-
-          return () => client.deactivate()
-    },[])
-
-    let sendButton
+  useEffect(() => {
     if (client) {
-        sendButton = <button onClick={e => {
-            for (let i = 0; i < 10; i++) {
-                client.publish({destination: '/topic/general', body: `Hello world ${i} ${uuidv4()}` });
-            }
-        }}>Send</button>
-    }
-    else {
-        sendButton = <p>no client available.</p>
-    }
+      const subscription = client.subscribe("/topic/general", (message) => {
+        emitter.next(message.body);
+      });
 
-    const messageList = messages.map(message => <li key={message.id}>{message.body}</li>)
+      return () => subscription.unsubscribe();
+    }
+  }, [client]);
 
-    return (
-        <>
-        <div>{s}</div>
-        <div>tick: {obsCount}</div>
-        <div>Stomp! {sendButton}</div>
-        <div>{messageList}</div>
-        </>
-    )
+  useEffect(() => {
+    const client = new Client({
+      brokerURL: "ws://localhost:61614",
+      connectHeaders: {},
+      debug: function (str: string) {
+        console.log(str);
+      },
+      reconnectDelay: 5000,
+      heartbeatIncoming: 4000,
+      heartbeatOutgoing: 4000,
+    });
+
+    client.onConnect = function (frame) {
+      setClient(client);
+    };
+
+    client.onStompError = function (frame) {
+      console.log("Broker reported error: " + frame.headers["message"]);
+      console.log("Additional details: " + frame.body);
+    };
+
+    client.activate();
+
+    return () => client.deactivate();
+  }, []);
+
+  return (
+    <>
+      <nav className="navbar" role="navigation" aria-label="main navigation">
+        <div className="navbar-start">
+          <div className="navbar-item">
+            <div className="buttons">
+              <a
+                className="button is-primary"
+                onClick={(e) => {
+                  if (client) {
+                    client.publish({
+                      destination: "/topic/general",
+                      body: `${obsCount}`,
+                    });
+                  }
+                }}
+              >
+                <strong>Publish message</strong>
+              </a>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <div className="container">
+        <div className="notification">
+          <table className="table is-bordered is-striped is-narrow is-hoverable">
+            <thead>
+              <tr>
+                <th>Property</th>
+                <th>Value</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Tick</td>
+                <td>{obsCount}</td>
+              </tr>
+              <tr>
+                <td>Received</td>
+                <td>{current}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default StompDemo
+export default StompDemo;
